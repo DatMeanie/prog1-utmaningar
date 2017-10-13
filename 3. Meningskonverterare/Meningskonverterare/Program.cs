79d8d05330c4c6382485e73799a308742d7e57ab
Albin Fyhr <albinfyhr@gmail.com>
2017-10-13 10:45:28 +0200

    två nya uppgifter klara
    meningskonverterare och talspyramid klar. easy peasey

---
+++ 3. Meningskonverterare/Meningskonverterare/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meningskonverterare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program converts your sentence to uppercase letters or lowercase letters");
+            Console.WriteLine("Type 1 for uppercase");
+            Console.WriteLine("Type 2 for lowercase");
+            Console.Write("Your Choice: ");
+            int choice = Convert.ToInt32(Console.ReadLine()); //WHAT WILL IT BE ???
+            if(choice == 1) //TO UPPER MAYBE?
+            {
+                Console.Write("Your Sentence: ");
+                string sentence = Console.ReadLine();
+                Console.WriteLine("Converted: " + sentence.ToUpper());
+            }
+            if (choice == 2) //only plebs take the low route
+            {
+                Console.Write("Your Sentence: ");
+                string sentence = Console.ReadLine();
+                Console.WriteLine("Converted: " + sentence.ToLower());
+            }
+            else {
+                Console.WriteLine("Wow man, that was not a choice. now you have to restart the program!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 4. Tal-pyramider/Tal-pyramider/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tal_pyramider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int turns = 0;
+
+            Console.WriteLine("Type in a number");
+            long number = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine(" ");
+            for (int i = 0; i < number; i += 1)
+            {
+               for(int j = 0; j < turns; j += 1)
+               {
+                    Console.Write(number);
+               }
+                Console.WriteLine(number);
+                turns++; 
+            }
+            Console.WriteLine("\n\nType anything to exit");
+            Console.ReadLine();
+        }
+    }
+}
