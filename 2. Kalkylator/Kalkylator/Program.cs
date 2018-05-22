3f6146a1966c9d12219fd0bed0da3c6ff9f9e203
Albin Fyhr <albinfyhr@gmail.com>
2018-05-22 09:21:41 +0200

    kommentering
    kommentarade utmaning 2

--- 2. Kalkylator/Kalkylator/Program.cs
+++ 2. Kalkylator/Kalkylator/Program.cs
@@ -8,11 +8,10 @@ namespace Kalkylator
 {
     class Program
     {
-        static string Kalkylatorn(int tal1, int tal2, string input)
+        static string Kalkylatorn(int tal1, int tal2, string input) //kalkylator funktion
         {
-            int completedTal = 0;
-            string resultat = "";
-            if (input == "1")
+            string resultat = ""; //håller ett tomt värde som ska fyllas senare
+            if (input == "1") //vilket räknesätt ska användas?
             {
                 resultat = $"{tal1} + {tal2} = {tal1 + tal2}";
             }
@@ -28,8 +27,7 @@ namespace Kalkylator
             {
                 resultat = $"{tal1} / {tal2} = {tal1 / tal2}";
             }
-
-            return resultat;
+            return resultat; //skickar resultatet
         }
         static void Main(string[] args)
         {
@@ -37,16 +35,16 @@ namespace Kalkylator
             while (69 != 1)
             {
                 Console.WriteLine("\nVilken räknesätt vill du använda?\n1. Addition\n2. Subtraktion\n3. Multiplikation\n4. Division");
-                string input;
-                while (69 != 1)
+                string input;   //vilket räknesätt?
+                while (69 != 1) //check om det funkar
                 {
                     input = Console.ReadLine();
                     if(input == "1" || input == "2" || input == "3" || input == "4")
                     { break; } else { Console.WriteLine("Använd numren satta på räknesätten"); }
                 }
                 Console.WriteLine("\nSkriv två number.");
-                int tal1Converted;
-                while (69 != 1)
+                int tal1Converted; //håller ett tomt värde som ska fyllas senare
+                while (69 != 1)//check om det funkar
                 {
                     Console.Write("Tal 1: ");
                     string tal1 = Console.ReadLine();
@@ -54,15 +52,15 @@ namespace Kalkylator
                     { break; } else { Console.WriteLine("Inte ett tal, försök igen"); }
                     
                 }
-                int tal2Converted;
-                while (69 != 1)
+                int tal2Converted; //håller ett tomt värde som ska fyllas senare
+                while (69 != 1)//check om det funkar
                 {
                     Console.Write("Tal 2: ");
                     string tal2 = Console.ReadLine();
                     if (Int32.TryParse(tal2, out tal2Converted)) { break; } else { Console.WriteLine("Inte ett tal, försök igen"); }
                 }
-                Console.WriteLine("\nResultat: " + Kalkylatorn(tal1Converted,tal2Converted, input));
-                
+                Console.WriteLine("\nResultat: " + Kalkylatorn(tal1Converted,tal2Converted, input));  //kalkylatorn används för att göra en beräkning
+
             }
         }
     }
